52f6112466cd4d7bc8b64182959fb0565a07ab5b
rotka <875151567@qq.com>
2024-09-07 00:00:59 +0800

    :sparkles:feat: 基于ScreenUtil 配置实现按比例适配屏幕

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import 'common/index.dart';
@@ -12,30 +13,37 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Flutter Demo',
-      // 样式
-      theme: AppTheme.light,
-      darkTheme: AppTheme.dark,
+    return ScreenUtilInit(
+      designSize: const Size(414, 896), // 设计稿中设备的尺寸(单位随意,建议dp,但在使用过程中必须保持一致)
+      splitScreenMode: false, // 支持分屏尺寸
+      minTextAdapt: false, // 是否根据宽度/高度中的最小值适配文字
+      builder: (context, child) {
+        return GetMaterialApp(
+          title: 'Flutter Demo',
 
-      // 主题
-      themeMode:
-          ConfigService.to.isDarkModel ? ThemeMode.dark : ThemeMode.light,
+          // 样式
+          theme: ConfigService.to.isDarkModel ? AppTheme.dark : AppTheme.light,
 
-      // 路由
-      initialRoute: RouteNames.stylesStylesIndex,
-      getPages: RoutePages.list,
-      navigatorObservers: [RoutePages.observer],
+          // 路由
+          initialRoute: RouteNames.stylesStylesIndex,
+          getPages: RoutePages.list,
+          navigatorObservers: [RoutePages.observer],
 
-      // 多语言
-      translations: Translation(), // 词典
-      localizationsDelegates: Translation.localizationsDelegates, // 代理
-      supportedLocales: Translation.supportedLocales, // 支持的语言种类
-      locale: ConfigService.to.locale, // 当前语言种类
-      fallbackLocale: Translation.fallbackLocale, // 默认语言种类
+          // builder
+          builder: (context, widget) {
+            // 使用 textScaler 进行文本缩放
+            return MediaQuery(
+              data: MediaQuery.of(context).copyWith(textScaler: const TextScaler.linear(1.0)),
+              child: widget!,
+            );
+          },
+
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
   }
+
 }
