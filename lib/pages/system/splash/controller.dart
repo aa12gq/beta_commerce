a1232c07a86aeaea006363513f8c2b684a2f20e1
rotka <875151567@qq.com>
2024-09-07 21:33:46 +0800

    :sparkles:feat: native 启动图

--- lib/pages/system/splash/controller.dart
+++ lib/pages/system/splash/controller.dart
@@ -1,9 +1,10 @@
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 
 class SplashController extends GetxController {
   SplashController();
 
-  _initData() {
+  initData() {
     update(["splash"]);
   }
 
@@ -17,7 +18,9 @@ class SplashController extends GetxController {
   @override
   void onReady() {
     super.onReady();
-    _initData();
+    // 删除设备启动图
+    FlutterNativeSplash.remove();
+    initData();
   }
 
   // @override
