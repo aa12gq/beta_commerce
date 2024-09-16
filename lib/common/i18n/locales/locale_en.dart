30e06ed4a49c4854dba22da2cafc2b2207e04a0c
rotka <875151567@qq.com>
2024-09-17 01:00:38 +0800

    :sparkles:feat: 商品界面初始，基本页面框架

--- lib/common/i18n/locales/locale_en.dart
+++ lib/common/i18n/locales/locale_en.dart
@@ -89,4 +89,13 @@ Map<String, String> localeEn = {
 
   // 商品 - 分类页
   LocaleKeys.gCategoryTitle: 'All Categories',
+
+  // 商品 - 详情
+  LocaleKeys.gDetailTitle: 'Product Info',
+  LocaleKeys.gDetailTabProduct: 'Product',
+  LocaleKeys.gDetailTabDetails: 'Details',
+  LocaleKeys.gDetailTabReviews: 'Reviews',
+  LocaleKeys.gDetailBtnAddCart: 'Add To Cart',
+  LocaleKeys.gDetailBtnBuy: 'Buy Now',
+
 };
---
+++ lib/pages/goods/product_details/widgets/index.dart
@@ -0,0 +1,5 @@
+library widgets;
+
+export './tab_product.dart';
+export './tab_reviews.dart';
+export './tab_detail.dart';
