0e5c76852f0913b7fa9790889f11ad2478251b4e
rotka <875151567@qq.com>
2024-09-24 22:59:31 +0800

    :sparkles:feat: 搜索栏防抖处理与推荐关键词列表实现

--- lib/pages/search/search_index/controller.dart
+++ lib/pages/search/search_index/controller.dart
@@ -1,7 +1,79 @@
+import 'package:beta_commerce/common/api/product.dart';
+import 'package:beta_commerce/common/models/beta/tags_model/tags_model.dart';
+import 'package:beta_commerce/common/models/request/product.dart';
+import 'package:beta_commerce/common/routers/name.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 
 class SearchIndexController extends GetxController {
   SearchIndexController();
+
+  // 搜索控制器
+  final TextEditingController searchEditController = TextEditingController();
+
+  // 搜索关键词
+  final searchKeyWord = "".obs;
+
+  // Tags 列表
+  List<TagsModel> tagsList = [];
+
+  // 搜索栏位 - 防抖
+  void searchDebounce() {
+    // getx 内置防抖处理
+    debounce(
+      // obs 对象
+      searchKeyWord,
+
+      // 回调函数
+          (value) async {
+        // 调试
+        if (kDebugMode) {
+          print("debounce -> $value");
+        }
+
+        // 拉取数据
+        await _loadSearch(value);
+        update(["search_index"]);
+      },
+
+      // 延迟 500 毫秒
+      time: const Duration(milliseconds: 500),
+    );
+
+    // 监听搜索框变化
+    searchEditController.addListener(() {
+      searchKeyWord.value = searchEditController.text;
+    });
+  }
+
+  /// 拉取数据
+  Future<bool> _loadSearch(String keyword) async {
+    if (keyword.trim().isEmpty == true) {
+      tagsList.clear();
+      return tagsList.isEmpty;
+    }
+
+    // 拉取数据
+    var result = await ProductApi.tags(TagsReq(
+      // 关键词
+      search: keyword,
+    ));
+
+    // 清空数据
+    tagsList.clear();
+
+    // 返回数据不为空
+    if (result.isNotEmpty) {
+      tagsList.addAll(result); // 添加数据
+    }
+
+    return tagsList.isEmpty;
+  }
+
+
+  // 列表项点击事件
+  void onListItemTap(TagsModel model) {}
 
   _initData() {
     update(["search_index"]);
@@ -17,11 +89,14 @@ class SearchIndexController extends GetxController {
   @override
   void onReady() {
     super.onReady();
-    _initData();
+    // 注册防抖
+    searchDebounce();
   }
 
-  // @override
-  // void onClose() {
-  //   super.onClose();
-  // }
+  @override
+  void onClose() {
+    super.onClose();
+    // 搜索控制器释放
+    searchEditController.dispose();
+  }
 }
