c48f45c87daef4ac1e5896f7781d46a31e8686bc
rotka <875151567@qq.com>
2024-09-24 23:12:45 +0800

    :sparkles:feat: 搜索顶部导航

--- lib/pages/search/search_filter/view.dart
+++ lib/pages/search/search_filter/view.dart
@@ -1,3 +1,5 @@
+import 'package:beta_commerce/common/components/appbar.dart';
+import 'package:beta_commerce/common/index.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -6,11 +8,24 @@ import 'index.dart';
 class SearchFilterPage extends GetView<SearchFilterController> {
   const SearchFilterPage({super.key});
 
+  // 搜索过滤栏
+  Widget _buildFilterBar() {
+    return const Text("搜索过滤栏");
+  }
+
+  // 数据列表
+  Widget _buildListView() {
+    return const Text("数据列表");
+  }
+
   // 主视图
   Widget _buildView() {
-    return const Center(
-      child: Text("SearchFilterPage"),
-    );
+    return <Widget>[
+      // 筛选栏
+      _buildFilterBar(),
+      // 数据列表
+      _buildListView(),
+    ].toColumn();
   }
 
   @override
@@ -20,12 +35,27 @@ class SearchFilterPage extends GetView<SearchFilterController> {
       id: "search_filter",
       builder: (_) {
         return Scaffold(
-          appBar: AppBar(title: const Text("search_filter")),
-          body: SafeArea(
-            child: _buildView(),
+          // 导航
+          appBar: mainAppBarWidget(
+            // 返回按钮
+            leading: ButtonWidget.icon(
+              IconWidget.icon(
+                Icons.arrow_back,
+                color: AppColors.primary,
+              ),
+              onTap: () => Get.back(),
+            ),
+            // 输入文字
+            hintText: LocaleKeys.searchPlaceholder.tr,
+            // 点击事件
+            onTap: () => Get.back(),
           ),
+
+          // 内容
+          body: _buildView(),
         );
       },
     );
   }
+
 }
